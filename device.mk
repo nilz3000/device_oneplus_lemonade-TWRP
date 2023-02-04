cf8c576645dbfb89b773a362fcac0a193a4438b3
niels <nielsraddatz@gmx.de>
2023-02-04 03:41:45 +0100

    fix vibration

--- device.mk
+++ device.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2021-2022 The LineageOS Project
 #
 #
-TW_DEVICE_VERSION := 4
+TW_DEVICE_VERSION := 5
 # AAPT
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
@@ -46,12 +46,11 @@ TW_USE_TOOLBOX := true
 TW_INCLUDE_RESETPROP := true
 TW_INCLUDE_REPACKTOOLS := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
-TW_Y_OFFSET := 95
-TW_H_OFFSET := -95
+TW_Y_OFFSET := 110
+TW_H_OFFSET := -110
 TWRP_INCLUDE_LOGCAT := true
 TARGET_USES_LOGD := true
 TARGET_USES_MKE2FS := true
-TW_NO_SCREEN_BLANK := true
 TW_HAS_EDL_MODE := true
 TW_EXCLUDE_APEX := true
 TW_CUSTOM_CPU_TEMP_PATH := "/sys/class/thermal/thermal_zone92/temp"
@@ -61,17 +60,16 @@ TW_BACKUP_EXCLUSIONS := /data/fonts/,/data/nandswap
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /config/usb_gadget/g1/functions/mass_storage.0/lun.%d/file
 TW_HAS_MTP := true
 
+#TW_SCREEN_BLANK_ON_BOOT := true
+TW_NO_SCREEN_BLANK := true
+
 TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel0-backlight/brightness"
 TW_DEFAULT_BRIGHTNESS := 1500
 TW_MAX_BRIGHTNESS := 4095
 
 TW_SUPPORT_INPUT_AIDL_HAPTICS := true
-#TW_NO_HAPTICS := true
 
-TW_LOAD_VENDOR_MODULES := "msm_drm.ko snd_event_dlkm.ko adsp_loader_dlkm.ko oplus_chg.ko touchscreen.ko oplus_bsp_tp_custom.ko swr_dlkm.ko swr_ctrl_dlkm.ko swr_haptics_dlkm.ko q6_dlkm.ko q6_notifier_dlkm.ko q6_pdr_dlkm.ko apr_dlkm.ko"
-
-# vibration
-#TW_LOAD_VENDOR_MODULES += "aw8697.ko haptic.ko"
+TW_LOAD_VENDOR_MODULES := "aw8697.ko adsp_loader_dlkm.ko oplus_chg.ko touchscreen.ko haptic.ko"
 
 #Properties
 TW_OVERRIDE_SYSTEM_PROPS := \
@@ -83,26 +81,23 @@ TARGET_RECOVERY_DEVICE_MODULES += libion \
     vendor.display.config@2.0.vendor \
     libdisplayconfig.qti \
     vendor.qti.hardware.vibrator.service.oplus \
-    vendor.qti.hardware.vibrator.service \
     libdrm.vendor \
 
 RECOVERY_BINARY_SOURCE_FILES += \
     $(TARGET_OUT_VENDOR_EXECUTABLES)/hw/vendor.qti.hardware.vibrator.service.oplus \
-    $(TARGET_OUT_VENDOR_EXECUTABLES)/hw/vendor.qti.hardware.vibrator.service \
 
 RECOVERY_LIBRARY_SOURCE_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
     $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/vendor.qti.hardware.vibrator.impl.oplus.so \
-    $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/vendor.qti.hardware.vibrator.impl.so \
     $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/libqtivibratoreffect.so \
     $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/vendor.display.config@2.0.so \
     $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/libdrm.so \
 
-#PRODUCT_COPY_FILES += \
+PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_RECOVERY)/root/system/etc/excluded-input-devices.xml \
     vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/etc/excluded-input-devices.xml
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/oneplus/sm8350-common/prebuilt-$(FIRMWARE_VERSION)/modules,$(TARGET_COPY_OUT_RECOVERY)/root/vendor/lib/modules)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*.ko,device/oneplus/sm8350-common/prebuilt-$(FIRMWARE_VERSION)/modules,$(TARGET_COPY_OUT_RECOVERY)/root/vendor/lib/modules/1.1)
 
 PRODUCT_COPY_FILES += \
     $(OUT_DIR)/target/product/lemonadep/system/etc/vintf/manifest.xml:$(TARGET_COPY_OUT_RECOVERY)/root/system/etc/vintf/manifest.xml \
