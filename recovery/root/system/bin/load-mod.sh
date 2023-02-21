e2c9faf300692247f1f203035c28a3bb3953a99d
niels <nielsraddatz@gmx.de>
2023-02-21 17:41:52 +0100

    add fstab entries to backup firmware

--- recovery/root/system/bin/load-mod.sh
+++ recovery/root/system/bin/load-mod.sh
@@ -2,6 +2,7 @@
 
 if /system/bin/grep -Fq twrpfastboot /proc/cmdline; then
     /system/bin/modprobe -d /vendor/lib/modules/1.1 msm_drm
+    #/system/bin/echo 1 > /proc/touchpanel/tp_fw_update
 fi
 
 
